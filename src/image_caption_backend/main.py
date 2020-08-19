6c60d3527e424a7b1d9a3780e8959cc91f3c8132
rishi <59619895+rishi-koul@users.noreply.github.com>
2020-08-19 18:26:29 -0400

    Update main.py

--- src/image_caption_backend/main.py
+++ src/image_caption_backend/main.py
@@ -7,16 +7,17 @@ import tensorflow.keras.applications.vgg16
 import tensorflow.keras.models
 from pyrebase import pyrebase
 
+# Please create a Google Firestore acc and copy your private apiKey. The format will be as follows
 config = {
 
-    "apiKey": "AIzaSyDRTG5YQkCLjWk8Wv9e7YzrWXFxQPL0BZA",
-    "authDomain": "fir-practice-a632b.firebaseapp.com",
-    "databaseURL": "https://fir-practice-a632b.firebaseio.com",
-    "projectId": "fir-practice-a632b",
-    "storageBucket": "fir-practice-a632b.appspot.com",
-    "messagingSenderId": "556202070812",
-    "appId": "1:556202070812:web:71a8428af7890d804aa0d9",
-    "measurementId": "G-YLTC014YK2"
+    "apiKey": ,
+    "authDomain": ,
+    "databaseURL": ,
+    "projectId": ,
+    "storageBucket": ,
+    "messagingSenderId": ,
+    "appId": ,
+    "measurementId": 
 
 }
 
